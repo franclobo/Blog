2c73d08f36d392dec90eba9f5fd6dc108fcfd266
franclobo <fjbl2788@gmail.com>
2022-09-01 22:42:11 -0500

    Add test for valid user with correct atributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
-  subject { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  subject { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', post_counter: 1) }
   before { subject.save }
+
+  it 'is valid with the correct attributes' do
+    expect(subject).to be_valid
+  end
 
   it 'name is present' do
     subject.name = nil
