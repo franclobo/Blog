e274c29900332d3ab89db0aad2bfe890f6d51367
franclobo <fjbl2788@gmail.com>
2022-09-06 17:09:20 -0500

    Fix linter errors

--- spec/feature/posts_controller_spec.rb
+++ spec/feature/posts_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 describe 'Posts', type: :feature do
   before(:all) do
-    @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', post_counter: 1)
+    @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+    bio: 'Teacher from Mexico.', post_counter: 1)
   end
   it 'should have a list of posts' do
     visit '/users/:user_id/posts'
