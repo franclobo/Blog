b7ce30917931d505f95946c8db5107d5854bdad6
franclobo <fjbl2788@gmail.com>
2022-09-06 17:11:50 -0500

    Fix linter errors

--- spec/feature/users_controllers_spec.rb
+++ spec/feature/users_controllers_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Users', type: :feature do
   before(:all) do
     @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-    bio: 'Teacher from Mexico.', post_counter: 1)
+                        bio: 'Teacher from Mexico.', post_counter: 1)
   end
   it 'should have a list of users' do
     visit '/users'
