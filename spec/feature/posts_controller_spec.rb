36f5c406957e9b2c1013e7feca5d55d169e1c21a
franclobo <fjbl2788@gmail.com>
2022-09-26 20:10:02 -0500

    Create Capybara tests for index

--- spec/feature/posts_controller_spec.rb
+++ spec/feature/posts_controller_spec.rb
@@ -11,4 +11,41 @@ describe 'Posts', type: :feature do
     visit '/users/1/posts'
     expect(page).to have_content('Tom')
   end
+  it 'I can see the user profile picture' do
+    visit '/users/1/posts'
+    expect(page).to have_css('img')
+  end
+  it 'I can see the user name' do
+    visit '/users/1/posts'
+    expect(page).to have_content('Tom')
+  end
+  it 'I can see the number of posts' do
+    visit '/users/1/posts'
+    expect(page).to have_content('0')
+  end
+  it 'I can see the post title' do
+    visit '/users/1/posts'
+    expect(page).to have_content('Hello')
+  end
+  it 'I can see the post text' do
+    visit '/users/1/posts'
+    expect(page).to have_content('This is my first post')
+  end
+  it 'I can see the first comment' do
+    visit '/users/1/posts'
+    expect(page).to have_content('No comments yet')
+  end
+  it 'I can see the number of comments' do
+    visit '/users/1/posts'
+    expect(page).to have_content('0')
+  end
+  it 'I can see the number of likes' do
+    visit '/users/1/posts'
+    expect(page).to have_content('0')
+  end
+  it 'When I click on a post, it redirects me to that post show page' do
+    visit '/users/1/posts'
+    click_link('Hello')
+    expect(page).to have_content('Hello')
+  end
 end
